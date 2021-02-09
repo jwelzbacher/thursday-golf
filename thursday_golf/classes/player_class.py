c45de0d4c973603dbe90217474527ea5fabade13
Jonathan Welzbacher <jwelzbacher@Jonathans-MacBook.local>
2021-02-09 11:24:35 -0600

    Add existing file

--- thursday_golf/classes/player_class.py
+++ thursday_golf/classes/player_class.py
@@ -1,17 +1,45 @@
 #player class
+from django import models
 
-class Player:
+class Player(models.Model):
+	"""docstring for Player"""
+	def __init__(self):
+		self.name = name
+        self.handicap = handicap
+        self.team = team
     
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.EmailField(blank=True, null=True)
+    phone_number = models.CharField(max_length=50, blank=True, null=True)
+    pdga_number = models.IntegerField(blank=True, null=True)
+    
+    def __str__(self):
+    	return f' {self.name} handicap is {self.handicap}.'
+		
+
+
+
+
+
+
+
+
+
+#Copied from dgscored/dgs/models.py 
+#class Player(models.Model):
+#    """
+ #   A model for a Human League player.
+    """
     def __init__(self,handicap):
         
-        self.name = name
-        self.handicap = []
-        
-    def remove_one(self):
-        return self.all_cards.pop(0)
-    
-    #def add_cards(self, new_cards):
-    	self.all_cards.append(new_cards)
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.EmailField(blank=True, null=True)
+    phone_number = models.CharField(max_length=50, blank=True, null=True)
+    pdga_number = models.IntegerField(blank=True, null=True)
+
     
     def __str__(self):
         return f'Player {self.name} handicap is {int(self.handicap)}.'
