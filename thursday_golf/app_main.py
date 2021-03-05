d8b2bcc56174b21b4d616284168613842c95b56f
Jonathan Welzbacher <jwelzbacher@Jonathans-MacBook.local>
2021-03-04 18:02:06 -0600

    removed base library to start a new Django project

--- thursday_golf/app_main.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import AbstractUser # To create a custom user model
-from django.core.validators import MaxValueValidator, MinValueValidator # To set minimum and maximum integer valuses
-from django.conf import settings
-from datetime import datetime, time, date
-from django.utils import timezone
-from django.db import models
